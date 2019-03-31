10ce1e182e403f4296cb70ce264f2d655b659a0f
Kazuki_Ouchi <kazukiouchi0714@gmail.com>
2019-03-31 16:18:02 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  get 'card/new'
+  get 'card/create'
+  devise_for :users
+  root 'top#index'
+  
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, except: %i(index) 
+  end
+end
